bf8c22dbed99bac1865ff37d55c66d2153bf55d1
ZachVillard <zdvillard@my.waketech.edu>
2026-09-16 22:00:48 +0000

    Weekly Goal Calculator created

--- js/task-manager.js
+++ js/task-manager.js
@@ -1,11 +1,6 @@
 function weeklyGoal(userName, dailyGoal, bonusTasks) {
     // Weekly Goal: Calculate the total weekly task goal for a user.
         
-    // Declare variables and values
-    userName = document.getElementById("myName").value;
-    dailyGoal = document.getElementById("dtg").value; 
-    bonusTasks = document.getElementById("wbt").value; 
-        
     // Output message to console
     console.log("Checking status for: " + userName); 
 
@@ -19,9 +14,15 @@ function weeklyGoal(userName, dailyGoal, bonusTasks) {
     let output = "User: " + userName + "<br>" + 
     "Total Weekly Goal: " + totalGoal + "<br>"; 
     document.getElementById("goal-message").innerHTML = output;
-    return output;
 };
-document.getElementById("goal-btn").addEventListener("click", function(event){
+const btn = document.getElementById("goal-btn");
+btn.getElementById("goal-btn").addEventListener("click", function(event){
     event.preventDefault();
-    weeklyGoal();
+
+    // Declare variables and values
+    userName = document.getElementById("myName").value;
+    dailyGoal = parseInt(document.getElementById("dtg").value); 
+    bonusTasks = parseInt(document.getElementById("wbt").value);
+
+    weeklyGoal(userName, dailyGoal, bonusTasks);
 });
