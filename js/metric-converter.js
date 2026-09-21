b6fec051ef6b5d197fce3837de92ea80f7a42357
ZachVillard <zdvillard@my.waketech.edu>
2026-09-21 22:27:58 +0000

    Metric-Converter

--- js/metric-converter.js
+++ js/metric-converter.js
@@ -10,7 +10,7 @@ function conversion(number) {
 
   var choice = document.getElementById("convert").selectedIndex;
 
-  if (choice === document.getElementsByTagName("in_To_cm").value) {
+  if (choice === document.getElementsByTagName("in_To_cm")[choice].value) {
     number = number * in_to_cm;
     number = number.toFixed(2);
   } else if (choice === document.getElementById("ft_To_cm").value) {
