6af1f3e6a0cfbdc06ef9eaf340d69c73663a7667
ZachVillard <zdvillard@my.waketech.edu>
2026-09-21 23:34:09 +0000

    Metric-Converter

--- js/metric-converter.js
+++ js/metric-converter.js
@@ -9,34 +9,43 @@ function conversion(number) {
   var km_to_mi = 0.62;
 
   var choice = document.getElementById("convert").selectedIndex;
+  let convertedOutput = "";
 
-  if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * in_to_cm;
-    number = number.toFixed(2);
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * ft_to_cm;
-    number = number.toFixed(2);
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * yd_to_m;
-    number = number.toFixed(2);
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * mi_to_km;
-    number = number.toFixed(2);
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * cm_to_in;
-    number = number.toFixed(2);
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * cm_to_ft;
-    number = number.toFixed(4); // Only this one uses 4 decimal places
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * m_to_yd;
-    number = number.toFixed(2);
-  } else if (choice === document.getElementsByTagName("option")[choice].value) {
-    number = number * km_to_mi;
-    number = number.toFixed(2);
+  if (choice === 0) {
+    let changedNumber = number * in_to_cm;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " inches is equal to " + changedNumber + " centimeters.";
+  } else if (choice === 1) {
+    let changedNumber = number * ft_to_cm;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " feet is equal to " + changedNumber + " centimeters.";
+  } else if (choice === 2) {
+    let changedNumber = number * yd_to_m;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " yards is equal to " + changedNumber + " meters.";
+  } else if (choice === 3) {
+    let changedNumber = number * mi_to_km;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " miles is equal to " + changedNumber + " kilometers.";
+  } else if (choice === 4) {
+    let changedNumber = number * cm_to_in;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " centimeters is equal to " + changedNumber + " inches.";
+  } else if (choice === 5) {
+    let changedNumber = number * cm_to_ft;
+    changedNumber = changedNumber.toFixed(4); // Only this one uses 4 decimal places
+    convertedOutput = number + " centimeters is equal to " + changedNumber + " feet.";
+  } else if (choice === 6) {
+    let changedNumber = number * m_to_yd;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " meters is equal to " + changedNumber + " yards.";
+  } else if (choice === 7) {
+    let changedNumber = number * km_to_mi;
+    changedNumber = changedNumber.toFixed(2);
+    convertedOutput = number + " kilometers is equal to " + changedNumber + " miles.";
   }
 
-  document.getElementById("convertOutput").innerHTML = number;
+  document.getElementById("convertOutput").innerHTML = convertedOutput;
 };
 
 const convertBtn = document.getElementById("convert-btn");
